de46648370494fe1ed9d123c9b5726bbec83b7d8
Nicholas Stees <nstees@gmail.com>
2016-04-06 22:24:36 -0400

    Finished testing

---
+++ fast_feedback/ajax.js
@@ -0,0 +1,23 @@
+(function($) {
+    Drupal.behaviors.myModule = {
+        'attach': function(context) {
+            $('a.feedback_no', context)
+                    .bind('click', function() {
+                $.get('/fast_feedback/downvote/' + parseInt(this.id, 10), null, successCallback);
+                return false;
+            });
+            $('a.feedback_yes', context)
+                    .bind('click', function() {
+                $.get('/fast_feedback/upvote/' + parseInt(this.id, 10), null, successCallback);
+                return false;
+            });
+        }
+    }
+
+    var successCallback = function(response) {
+        console.log(response);
+        // var result = JSON.parse(response);
+        // console.log(result);
+        $('div#feedback').html('This page has an average rating of '+Math.round(response.rating)+"%");
+    }
+})(jQuery);
